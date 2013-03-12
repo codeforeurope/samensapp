194fe5803470cedbb2833d744eca48b575e8c9a0
Besma M'charek <bmcharek@gmail.com>
2013-03-12 14:04:38 +0100

    just testing new key

--- app/models/building.rb
+++ app/models/building.rb
@@ -1,3 +1,4 @@
+#This is the building eg: Meevaart
 class Building < ActiveRecord::Base
   attr_accessible :adress, :name
   belongs_to :organization
--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-
-
-
-ActiveRecord::Schema.define(:version => 20130312124037) do
+ActiveRecord::Schema.define(:version => 20130312125159) do
 
   create_table "buildings", :force => true do |t|
     t.string   "name"
     t.text     "adress"
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
+    t.integer  "organization_id"
   end
 
   create_table "organizations", :force => true do |t|
