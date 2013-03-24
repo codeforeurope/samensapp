bb2d86b92d48977339378e96927c60d8d497ce1f
Giovanni Maggini <giovanni.maggini@gmail.com>
2013-03-24 12:42:41 +0100

    Added the modified schema.rb after migrate

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,13 @@
 
 ActiveRecord::Schema.define(:version => 20130322161224) do
 
+  create_table "attachments", :force => true do |t|
+    t.string   "description"
+    t.string   "file"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
+
   create_table "booking_requests", :force => true do |t|
     t.datetime "start_at"
     t.datetime "end_at"
