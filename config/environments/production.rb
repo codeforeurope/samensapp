b96d28c99caff6a9f3d9b81b76de0813bdcebe34
Piotr Steininger <piotr.steininger@gmail.com>
2013-03-25 23:13:01 +0100

    Fixed configuration issues

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,5 +65,5 @@ Samensapp::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
-  config.action_mailer.default_url_options = { :host => 'http://demeevaart.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'demeevaart.herokuapp.com' }
 end
