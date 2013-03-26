6bbca1802e467fb69ee0f70e8acb87eb4104084f
Giovanni Maggini <giovanni.maggini@gmail.com>
2013-03-26 18:51:35 +0100

    Booking Request list implemented. Booking agent can assign a request to him/herself.

--- app/models/booking_request.rb
+++ app/models/booking_request.rb
@@ -1,7 +1,7 @@
 class BookingRequest < ActiveRecord::Base
   STATUSES = %w"locked submitted accepted canceled awaiting_reply"
   attr_accessible :catering_needs, :description, :equipment_needs, :notes, :people, :submitter_id,
-                  :organization_name, :contact_person, :contact_email, :contact_phone, :organization_address, :start_time, :end_time, :event_date
+                  :organization_name, :contact_person, :contact_email, :contact_phone, :organization_address, :start_time, :end_time, :event_date, :assignee_id
 
   #attr_accessor :start_time, :end_time, :event_date
   #attr_writer :event_date
@@ -11,7 +11,6 @@ class BookingRequest < ActiveRecord::Base
 
   belongs_to :submitter, :class_name => 'User', :foreign_key => :submitter_id
   belongs_to :booking_agent, :class_name => 'User', :foreign_key => :assignee_id
-  has_one :booking_status
   #has_many :events
 
   accepts_nested_attributes_for :submitter
@@ -19,10 +18,6 @@ class BookingRequest < ActiveRecord::Base
   validates_presence_of :submitter, :catering_needs, :description, :equipment_needs, :people
   validates_presence_of :contact_email, :contact_person, :contact_phone, :organization_address
   validates_associated :submitter, :on => :create
-  validates_presence_of :status, :on => :create
-
-
-
 
 
   def start_time
@@ -88,7 +83,7 @@ class BookingRequest < ActiveRecord::Base
   end
 
   def set_default_status
-    self.status = self::STATUSES[1]
+    self.status = STATUSES[1]
   end
 
 end
