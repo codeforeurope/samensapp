833f2d2e65df838eddd950548375e54eb470a54d
Giovanni Maggini <giovanni.maggini@gmail.com>
2013-03-26 22:08:12 +0100

    Added some translations (english) to booking requests pages

--- app/helpers/booking_requests_helper.rb
+++ app/helpers/booking_requests_helper.rb
@@ -2,7 +2,7 @@ module BookingRequestsHelper
 
   def format_assignee(assignee_id)
     if assignee_id.nil?
-      t("None")
+      t(:booking_request_helper.tooltip_none, :default => "None")
     else
       User.find(assignee_id).name
     end
@@ -18,15 +18,19 @@ module BookingRequestsHelper
 
   def format_status_tooltip(status)
     if status.nil?
-      t("None")
+      t(:booking_request_helper.tooltip_none, :default => "None")
     else
       case status
         when "submitted"
-          "Request has been received, but not processed yet."
-        when "locked"
-          "A Booking Agent is working on it, right now."
+          t(:booking_request_helper.tooltip_request_submitted, :default => "Request has been received, but not processed yet.")
+        when "assigned"
+          t(:booking_request_helper.tooltip_request_assigned, :default => "A Booking Agent is working on it, right now.")
+        when "completed"
+          t(:booking_request_helper.tooltip_request_completed, :default => "An offer has been sent for this request.")
+        when "canceled"
+          t(:booking_request_helper.tooltip_request_canceled, :default => "The request has been canceled.")
         else
-          "Working on it"
+          t(:booking_request_helper.tooltip_request_working, :default => "Working on it.")
       end
     end
   end
