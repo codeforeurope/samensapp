31cea49db695d976e66e655f20a0801f1595be2a
Giovanni Maggini <giovanni.maggini@gmail.com>
2013-07-24 00:01:16 +0200

    updated schema after db migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-
-ActiveRecord::Schema.define(:version => 20130710152212) do
+ActiveRecord::Schema.define(:version => 20130711142325) do
 
   create_table "booking_requests", :force => true do |t|
     t.datetime "start_at"
